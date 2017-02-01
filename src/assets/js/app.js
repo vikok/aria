162ee746dd4ec8b61c2dd0fd4cad1ba3901c8e43
vikok <vikskopkop@gmail.com>
2017-02-01 18:57:16 +0200

    change all

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,8 +1,25 @@
-$(document).foundation();
-$('.ba-slider').slick({
-	dots: true
-});
-$('.ba-slider-2').slick({
-	dots: true,
-	variableWidth: true
-});
+
+;(function() {
+  $(document).foundation();
+  $('.ba-slider').slick({
+  	dots: true
+  });
+  $('.ba-slider-2').slick({
+  	dots: true,
+  	variableWidth: false
+  });
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 39.889965, lng: 18.110712},
+    scrollwheel: false,
+    zoom: 14
+  });
+}
+  initMap();
+  var torreMarker = new google.maps.Marker({
+      position: map,
+      map: map,
+      title: "Torre",
+      icon: 'assets/img/contact/marker.png'
+      });
+})();
